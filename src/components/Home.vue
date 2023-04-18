d68fd0fe2aea4cf3bbcae0d5a392a838abdfe12a
Silvia M. Herguedas <silviaherguedas@gmail.com>
2023-04-18 10:42:04 +0200

    Composition API: Uso de props con script setup

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,23 +1,23 @@
 <template>
-  <div>Fullname: {{ fullName }}</div>
+  {{ fullName }}
 </template>
 
-<script>
-import { toRefs, computed } from "vue";
-export default {
-  props: {
-    firstName: String,
-    lastName: String,
-  },
-  setup(props) {
-    const { firstName, lastName } = toRefs(props);
+<script setup>
+import { computed, toRefs, watch } from "vue";
 
-    const fullName = computed(() => {
-      return `${firstName.value} ${lastName.value}`;
-    });
-    return {
-      fullName,
-    };
-  },
-};
+const props = defineProps({
+  firstName: String,
+  lastName: String,
+});
+
+const { firstName, lastName } = toRefs(props);
+
+const fullName = computed(() => {
+  return `${firstName.value} ${lastName.value}`;
+});
+
+watch(firstName, (newValue, oldValue) => {
+  console.log(`The new name is ${newValue}`);
+  console.log(`The old name is ${oldValue}`);
+});
 </script>
