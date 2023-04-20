5c74cfbc30390a92b8812c34ef6cbaf7a86f6b7e
Silvia M. Herguedas <silviaherguedas@gmail.com>
2023-04-20 09:55:57 +0200

    Composition API: Uso de Script Setup

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
-<script setup>
-import Home from "./components/Home.vue";
-import { provide, reactive } from "vue";
+<template>
+  <Home first-name="Diana" last-name="Martinez"></Home>
+</template>
 
-const persona = reactive({
-  firstName: "Pepe",
-  lastName: "Argento",
-});
-
-provide("objPerson", persona);
+<script>
+import Home from "@/components/Home.vue";
+import { provide } from "vue";
+export default {
+  name: "App",
+  components: {
+    Home,
+  },
+  setup() {
+    provide("username", "DianaNerd");
+  },
+};
 </script>
 
-<template>
-  <Home />
-</template>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,37 @@
-<script setup>
-import { inject, onMounted, ref } from "vue";
-
-const { firstName, lastName } = inject("objPerson");
-
-//accede al elemento [h1]
-const elem = ref(null);
-onMounted(() => console.log("elem:", elem.value));
-</script>
-
 <template>
-  <h1 ref="elem">Full Name: {{ firstName + " " + lastName }}</h1>
+  <div>{{ fullName }}</div>
+  <div>{{ username }}</div>
+  <button ref="btn">Click!</button>
 </template>
+
+<script setup>
+import {
+  defineProps,
+  defineExpose,
+  ref,
+  toRefs,
+  computed,
+  watch,
+  inject,
+} from "vue";
+
+const props = defineProps({
+  firstName: String,
+  lastName: String,
+});
+const { firstName, lastName } = toRefs(props);
+const fullName = computed(() => {
+  return `${firstName.value} ${lastName.value}`;
+});
+const username = inject("username");
+
+defineExpose({
+  fullName,
+});
+const btn = ref(null);
+console.log(btn.value);
+
+watch(btn, (valor) => {
+  console.log(valor);
+});
+</script>
